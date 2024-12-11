e0ecc967b0ee5a386d8e4f9fe5a98e0e0f103c3e
jijisreejesh <jijibsc@gmail.com>
2024-12-11 12:02:33 +0530

    sortremove'

--- src/components/stores/cart.js
+++ src/components/stores/cart.js
@@ -4,24 +4,12 @@ import { defineStore } from 'pinia';
 export const useCartStore=defineStore("cart",()=>{
     const cartItems=ref([]);
 
-    //getter
-    const sortItems=computed(()=>{
-        [...cartItems.value].sort((a,b)=>b.id-a.id)
-    })
-
     //actions
     const addItems=(item)=>{
-        //find object whose id similar to item
-        // console.log("AddItems method working");
-        
-        // const checkItems=cartItems.value.find((x)=>x.id===item.id)
-        // if(!checkItems){
-        //     cartItems.value.push(item)
-        // }
         cartItems.value.push(item)
     }
     const removeItems=(id)=>{
         cartItems.value=cartItems.value.filter((x)=>x.id!==id)
     }
-    return {cartItems,addItems,sortItems,removeItems};
+    return {cartItems,addItems,removeItems};
 });
